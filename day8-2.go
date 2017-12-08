7293c8aa23e916989fd78158839c395a588fe6df
Matt Bargenquast <matt.bargenquast@gmail.com>
2017-12-08 20:14:13 +1100

    day 8 part 2

---
+++ day8-2.go
@@ -0,0 +1,112 @@
+package main
+
+import (
+	"os"
+	"bufio"
+	"regexp"
+	"strconv"
+	"fmt"
+)
+
+type Comparator string
+const (
+	GT Comparator = ">"
+	LT Comparator = "<"
+	GTE Comparator = ">="
+	LTE Comparator = "<="
+	EQ Comparator = "=="
+	NE Comparator = "!="
+)
+
+var registers map[string]int = make(map[string]int,30)
+
+func fill_registers(f string) {
+	file, _ := os.Open(f)
+	s := bufio.NewScanner(file)
+
+	r_reg, _ := regexp.Compile(`(\w+).+ if (\w+) `)
+
+	for s.Scan() {
+		line := s.Text()
+		matches := r_reg.FindStringSubmatch(line)
+		if matches != nil {
+			registers[matches[1]] = 0
+			registers[matches[2]] = 0
+		}
+	}
+}
+
+func test_instruction(reg string, c Comparator, v int) bool {
+	switch c {
+	case GT:
+		return registers[reg] > v
+	case LT:
+		return registers[reg] < v
+	case GTE:
+		return registers[reg] >= v
+	case LTE:
+		return registers[reg] <= v
+	case EQ:
+		return registers[reg] == v
+	case NE:
+		return registers[reg] != v
+	}
+	return false
+}
+
+func process_instructions(f string) int {
+	file, _ := os.Open(f)
+	s := bufio.NewScanner(file)
+
+	r_inst, _ := regexp.Compile(`(\w+) (inc|dec) (-?\d+) if (\w+) (.+) (-?\d+)`)
+
+	largest_val := 0
+
+	for s.Scan() {
+		line := s.Text()
+		matches := r_inst.FindStringSubmatch(line)
+		if matches != nil {
+			op_reg := matches[1]
+			op_inst := matches[2]
+			op_val, _ := strconv.Atoi(matches[3])
+			op_comp_reg := matches[4]
+			op_comp := matches[5]
+			op_comp_val, _ := strconv.Atoi(matches[6])
+
+			if test_instruction(op_comp_reg, Comparator(op_comp), op_comp_val) {
+				if op_inst == "inc" {
+					registers[op_reg] += op_val
+				} else if op_inst == "dec" {
+					registers[op_reg] -= op_val
+				} else {
+					fmt.Printf("Invalid instruction: %s\n", op_inst)
+				}
+				if registers[op_reg] > largest_val {
+					largest_val = registers[op_reg]
+				}
+			}
+		} else {
+			fmt.Println("Did not match")
+		}
+	}
+
+	return largest_val
+}
+
+func find_largest_val() int {
+	largest := 0
+	for _, v := range(registers) {
+		if v > largest {
+			largest = v
+		}
+	}
+	return largest
+}
+
+func main() {
+	datafile := os.Args[1]
+	fill_registers(datafile)
+	largest_in_proc := process_instructions(datafile)
+	fmt.Printf("Largest ever value: %d", largest_in_proc)
+}
+
